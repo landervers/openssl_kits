02527b1c06d28a90e449ef1675e0c768c63eb7a7
landervers <laihj111@gmail.com>
2024-10-15 20:55:30 +0800

    rsa修改

---
+++ my_openssl_kits/main.cpp
@@ -0,0 +1,7 @@
+#ifndef OPENSSL_MIAN_
+#include "my_des.h"
+#include "my_rsa.h"
+int main() {
+	return 0;
+}
+#endif // !OPENSSL_MIAN_
---
+++ my_openssl_kits/my_rsa.cpp
@@ -0,0 +1,23 @@
+#include "my_rsa.h"
+
+#define KEY_LENGTH  2048             // 密钥长度
+#define PUB_KEY_FILE "pubkey.pem"    // 公钥路径
+#define PRI_KEY_FILE "prikey.pem"    // 私钥路径
+
+void GenerateRSAkey() {
+	size_t prikey_len = 0;
+	size_t pubkey_len = 0;
+	char *prikey = nullptr;
+	char* pubkey = nullptr;
+}
+
+void RsaDecrypt() {
+
+}
+void RsaEncrypt() {
+
+}
+
+void rsa_Kit_test() {
+
+}
